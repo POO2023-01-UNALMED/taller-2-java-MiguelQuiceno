b9b9b45a02186c9ff4589eecb16d2c04204a83be
Miguel Quiceno <miguelangel01yo@gmail.com>
2023-02-17 16:52:27 -0500

    resto

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,40 @@
+package test;
+public class Auto {
+    String modelo;
+    int precio;
+    Asiento[] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados;
+
+    int cantidadAsientos(){
+        int a = 0
+        for(int i = 0; i <= this.asientos.length; i++){
+            if(this.asientos[i] != null){
+                a++;
+            }
+        }
+        return a;
+        
+    }
+    String verificarIntegridad(){
+        boolean f = false;
+        if(this.registro == this.motor.registro){
+            f = true;
+            for(int i = 0; i <= this.asientos.length; i++){
+                if(this.asientos[i] != null){
+                    if(this.asientos[i] != this.registro){
+                        f = false;
+                    }
+                }
+            }
+        }
+        if(f){
+            return "Auto original";
+        }else{
+            return "Las piezas no son originales";
+        }
+    }
+    
+}
